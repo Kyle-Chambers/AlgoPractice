7f8524ae139521d3b2bff49b233a03ef121af3dd
Kyle Chambers <kylejchambers@gmail.com>
2018-05-14 17:46:06 -0700

    passing tests for reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,25 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+
+// function reverseString(str){
+//     return str.split('').reverse().join('');
+// }
+
+// function reverseInt(n) {
+//     return n < 0 
+//         ? - (Number(reverseString(n.toString().slice(1)))) 
+//         : (Number(reverseString(n.toString())));
+// }
+
+function reverseInt(n) {
+    let reversed = n
+        .toString()
+        .split('')
+        .reverse()
+        .join('');
+
+    return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
