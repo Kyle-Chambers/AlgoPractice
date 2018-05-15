931e585e65e94812f6671482946cf4d074b8598a
Kyle Chambers <kylejchambers@gmail.com>
2018-05-14 18:40:20 -0700

    passing tests for maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charCount = {};
+    let mostRepeated = '';
+    let max = 0;
+
+    for (let char of str){
+        charCount[char] = charCount[char]++ || 1;
+    }
+
+    for (let key in charCount){
+        if (charCount[key] > max){
+            max = charCount[key];
+            mostRepeated = key;
+        }
+    }
+
+    return mostRepeated;
+}
 
 module.exports = maxChar;
